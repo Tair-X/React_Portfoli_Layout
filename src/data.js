f376655def25d00c383623566f7ec0bffae93666
Tair-X <you@example.com>
2021-10-09 23:56:25 +0300

    lol

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://source.unsplash.com/random",
+    link: "https://github.com/Tair-X/",
+  },
+  {
+    id: 2,
+    img: "https://picsum.photos/200/300",
+    link: "https://github.com/Tair-X/",
+  },
+  {
+    id: 3,
+    img: "https://random.imagecdn.app/200/300",
+    link: "https://github.com/Tair-X/",
+  },
+  {
+    id: 4,
+    img: "https://loremflickr.com/320/240",
+    link: "https://github.com/Tair-X/",
+  },
+  {
+    id: 5,
+    img: "https://source.unsplash.com/random",
+    link: "https://github.com/Tair-X/",
+  },
+  {
+    id: 6,
+    img: "https://source.unsplash.com/random",
+    link: "https://github.com/Tair-X/",
+  },
+];
